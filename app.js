f616aa9da7b6b6eb4e820f3f89aefe0ce0670881
Daryl Hewett <86888707+darylchewett@users.noreply.github.com>
2022-12-01 12:27:03 +0000

    Project init

---
+++ app.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+const progToRun = prompt('Which day would you like to run?');
+
+console.log(`Running day ${progToRun}....`)
+
+//TODO: Call the appropriate file
