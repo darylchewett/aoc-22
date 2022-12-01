2b066aadb727d19a08ef2244816571cf3c79f9a8
Daryl Hewett <86888707+darylchewett@users.noreply.github.com>
2022-12-01 16:54:02 +0000

    Day1 Part 2 Complete

---
+++ code/day1_part2.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const readline = require('readline');
+
+let array = [];
+let currentCount = 0;
+async function processLines(){
+
+    const fileStream = fs.createReadStream('inputs/day_1.txt')
+
+    const rl = readline.createInterface({
+    input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    for await (const lineValue of rl){
+
+        //Line is not blank it's part of the current or new total
+        if (lineValue !== ""){
+            currentCount += parseInt(lineValue);
+            continue;
+        }
+
+        // If it's blank, push the total to the array
+        array.push(currentCount);
+
+
+        //reset the current count, a blank line means we're starting a new list on next loop
+        currentCount = 0;
+    }
+
+    //End off the loop, sort the values
+
+    array.sort((a,b)=>{return b - a})
+
+    console.log(array)
+
+    console.log(array[0]+array[1]+array[2])
+
+}
+
+processLines();
