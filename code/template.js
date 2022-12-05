72e3de36f8b9647ac98576098e205d9b2e358253
Daryl Hewett <86888707+darylchewett@users.noreply.github.com>
2022-12-05 15:02:01 +0000

    added template for days that calls read in and wraps async task

---
+++ code/template.js
@@ -0,0 +1,11 @@
+const inputReader = require('./input_reader.js')
+//Moved input reader into reusable module that returns an array of the data
+
+async function task() {
+
+    const puzzleInput = await inputReader.processInput('');
+
+
+}
+
+task();
